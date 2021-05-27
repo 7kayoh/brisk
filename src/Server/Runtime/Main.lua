8d48434209019263edab47325684370963ea5371
va1kio <40730127+va1kio@users.noreply.github.com>
2021-05-27 22:12:59 +0800

    Added Services

--- src/Server/Runtime/Main.lua
+++ src/Server/Runtime/Main.lua
@@ -3,6 +3,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local LOG_PREFIX = "Brisk; "
 local MODULE_CHECK_ERROR = "File \"(REP)\" is not a ModuleScript"
+local EXPECT_GOT = "Argument \"(REP)\" expects type \"(REP)\", got type \"(REP)\""
 
 local Server = script.Parent
 local ServerLibrary = Server.Library
@@ -11,10 +12,18 @@ local Shared = ReplicatedStorage.SharedBrisk
 local Logger = require(Shared.Logger)
 local Maid = require(Shared.Maid)
 local Promise = require(Shared.Promise)
+local Services = require(ServerLibrary.Services)
 
 local function InitializeEnvironments(module)
-    assert(module:IsA("ModuleScript"), string.gsub(MODULE_CHECK_ERROR , "(REP)", ""))
+    module.LOG_PREFIX = LOG_PREFIX
+    module.EXPECT_GOT = EXPECT_GOT
+    module.Server = Server
+    module.Shared = Shared
+    module.Logger = Logger
+    module.Maid = Maid
+    module.Promise = Promise
 end
 
+InitializeEnvironments(Services)
 
-return {}
+return nil
