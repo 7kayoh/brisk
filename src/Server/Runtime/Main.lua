9881600ba5e002103fda930aeb020a46862cd198
Bobo <71311544+SilentsReplacement@users.noreply.github.com>
2021-05-27 18:23:00 +0500

    Update Main.lua

--- src/Server/Runtime/Main.lua
+++ src/Server/Runtime/Main.lua
@@ -2,30 +2,23 @@
 -- @7kayoh
 -- The core of Brisk for the server end
 
--- Singletons
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
--- Constants
 local LOG_PREFIX = "Brisk; "
-local IS_NOT_MODULESCRIPT = "File \"(REP)\" is not a ModuleScript"
+local MODULE_CHECK_ERROR = "File \"(REP)\" is not a ModuleScript"
 
--- Variables
 local Server = script.Parent
-local ServerLibrary = Server:FindFirstChild("Library")
-local Shared = ReplicatedStorage:FindFirstChild("SharedBrisk")
+local ServerLibrary = Server.Library
+local Shared = ReplicatedStorage.SharedBrisk
 
-local Logger = require(Shared:FindFirstChild("Logger"))
-local Maid = require(Shared:FindFirstChild("Maid"))
-local Promise = require(Shared:FindFirstChild("Promise"))
+local Logger = require(Shared.Logger)
+local Maid = require(Shared.Maid)
+local Promise = require(Shared.Promise)
 
--- Functions
-local function InitializeEnvironments(Module)
-    assert(Module:IsA("ModuleScript"), string.gsub(IS_NOT_MODULESCRIPT, "(REP)", ""))
+local function InitializeEnvironments(module)
+    assert(module:IsA("ModuleScript"), string.gsub(MODULE_CHECK_ERROR , "(REP)", ""))
 end
 
--- Rules
-
--- Executables
 
 return {}
